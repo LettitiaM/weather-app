5e615321a7d6343f0da79d220cd9bccb5d5e73d5
lettitia.mokubung@dadevs.co.za <lettitia.mokubung@dadevs.co.z>
2024-09-06 10:25:38 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Weather from "./Weather"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Weather App
-
+      <div className='container'>
+        <Weather/>
+        <h1>Weather App</h1>
+        <footer>This project was coded by {" "} <a href=''>Lettitia</a> and is open source on {" "}<a href=''>Github</a></footer>
+      </div>
     </div>
   );
 }
