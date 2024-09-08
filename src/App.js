3dcc3de4950f5bebdfbccac67ba0658275d25c45
lettitia.mokubung@dadevs.co.za <lettitia.mokubung@dadevs.co.z>
2024-09-08 13:01:28 +0200

    changes href

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <div className='container'>
         <h1>Weather App</h1>
         <Weather defaultCity="Johannesburg"/>
-        <footer>This project was coded by {" "} <a href=''>Lettitia</a> and is open source on {" "}<a href=''>Github</a></footer>
+        <footer>This project was coded by {" "} <a href='www.linkedin.com/in/lettitia-mokubung-6412b5180
+
+'>Lettitia</a> and is open source on {" "}<a href='https://github.com/LettitiaM/weather-app'>Github</a></footer>
       </div>
     </div>
   );
