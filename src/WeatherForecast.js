e21c433f65c20a09fbb6a092034a7fb95f7831de
lettitia.mokubung@dadevs.co.za <lettitia.mokubung@dadevs.co.z>
2024-09-12 18:55:23 +0200

    added use effect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import WeatherForecastDay from "./WeatherForecastDay"
 import "./WeatherForecast.css"
 import axios from "axios";
@@ -12,8 +12,10 @@ export default function WeatherForecast(props){
         setLoaded(true)
         setForecast(response.data.daily)
     }
+    useEffect(()=>
+    {setLoaded(false);},[props.coordinates]
+);
 
-    
     if (loaded){
        return(
         <div className="WeatherForecast">
